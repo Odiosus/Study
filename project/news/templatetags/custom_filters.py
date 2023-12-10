1e696de9eb098820c886389e6a764868cf6b2909
I-am-told <artyomkolesnikov1990@gmail.com>
2023-12-10 19:05:20 +0200

    HW-03

--- project/news/templatetags/custom_filters.py
+++ project/news/templatetags/custom_filters.py
@@ -2,7 +2,18 @@ from django import template
 
 register = template.Library()
 
+censor_list = ['смерть', 'убить', 'оружие', ]
+
+# Слово "смерти" встречается в заголовке поста про шимпанзе
+# Слово "убить" встречается в тексте этого же поста
+# Слово "оружия" встречается в посте про вселенную
 
 @register.filter()
 def censor(text):
-    return text
+    text_list = text.split()
+    for i, j in enumerate(text.split()):
+        for k in censor_list:
+            if j.find(k[:-1]) != -1:
+                text_list[i] = len(text_list[i]) * '*'
+    return ' '.join(text_list)
+
