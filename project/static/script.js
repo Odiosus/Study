e125979748367fecf13bec1085bc0ae82da5e12c
I-am-told <artyomkolesnikov1990@gmail.com>
2024-01-20 19:30:03 +0200

    design main page - done

--- project/static/script.js
+++ project/static/script.js
@@ -1,37 +1,26 @@
 
-
 document.addEventListener('DOMContentLoaded', () => {
+
     const buttons = document.querySelectorAll('.btn');
     const activebutid = window.sessionStorage.getItem('activebutton');
-    const aConts = document.querySelectorAll('.aCont');
-    const activeAId = window.sessionStorage.getItem('activeaCont');
 
     let lastactive = activebutid
         ? document.getElementById(activebutid)
         : null;
 
-    let lastactive1 = activeAId
-        ? document.getElementById(activeAId)
-        : null;
-
     lastactive?.classList.add('active');
 
     function clickhandler() {
+
         lastactive?.classList.remove('active');
-        lastactive1?.classList.remove('activea');
-        this.classList.add('active');
+        window.sessionStorage.removeItem('activeaCont');
         window.sessionStorage.setItem('activebutton', this.id);
-        window.sessionStorage.setItem('activeaCont', this.id);
     }
 
     for (const but of buttons) {
         but.addEventListener('click', clickhandler);
     }
 
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
     const aConts = document.querySelectorAll('.aCont');
     const activeAId = window.sessionStorage.getItem('activeaCont');
 
@@ -42,6 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
     lastactive1?.classList.add('activea');
 
     function clickhandler1() {
+
         lastactive1?.classList.remove('activea');
         this.classList.add('activea');
         window.sessionStorage.setItem('activeaCont', this.id);
@@ -52,7 +42,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 });
-
-document.getElementById('delbN').onclick = function(e){
-  window.sessionStorage.removeItem('activebutton', this.id);
-}
