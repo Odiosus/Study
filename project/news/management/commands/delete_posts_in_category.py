805bfca6a040e1548e6df0c46135adf906186520
I-am-told <artyomkolesnikov1990@gmail.com>
2024-02-12 22:48:14 +0200

    update command delete_posts_in_category.py

--- project/news/management/commands/delete_posts_in_category.py
+++ project/news/management/commands/delete_posts_in_category.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from news.models import Post, Category
 
 
@@ -9,15 +9,18 @@ class Command(BaseCommand):
         parser.add_argument('category', type=str)
 
     def handle(self, *args, **options):
-        answer = input(f'Do you really want to delete all posts from cateroty {options["category"]}? yes/no   ')
-
-        if answer != 'yes':
-            self.stdout.write(self.style.ERROR('cancel'))
-            return
-        try:
+        if Category.objects.filter(name=options['category']).exists():
             category = Category.objects.get(name=options['category'])
-            Post.objects.filter(category=category).delete()
-            self.stdout.write(self.style.SUCCESS(f'Succesfully deleted all posts from category {category.name}'))
-        except Post.DoesNotExist:
-            self.stdout.write(self.style.ERROR(f'Could not find category {category}'))
+            answer = input(f'Do you really want to delete all posts from category {options["category"]}? yes/no   ')
+            if answer != 'yes':
+                self.stdout.write(self.style.ERROR('cancel'))
+                return
+            posts = Post.objects.filter(category=category)
+            if posts:
+                posts.delete()
+                self.stdout.write(self.style.SUCCESS(f'Successfully deleted all posts from category {options["category"]}'))
+            else:
+                self.stdout.write(self.style.ERROR(f'Could not find posts from category {options["category"]}'))
+        else:
+            self.stdout.write(self.style.ERROR(f'Could not find category {options["category"]}'))
 
